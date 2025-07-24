cb3b73a1eb572b39c79cd80cf5676743b37c9e8b
npoddar <npoddar@nef-devel2.inria.fr>
2025-07-24 13:52:30 +0200

    head body done

--- mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
+++ mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
@@ -100,4 +100,4 @@ optimizer = dict(
 )
 # runtime settings
 checkpoint_config = dict(interval=5)
-work_dir = './work_dirs/manet_prova_sgp_six_classes_embedding_average_our_model/'
+work_dir = './work_dirs/experts/head_body_without_word_embedding'
--- mar_scripts/manet/mmaction2/mmaction/datasets/video_dataset.py
+++ mar_scripts/manet/mmaction2/mmaction/datasets/video_dataset.py
@@ -59,29 +59,39 @@ class VideoDataset(BaseDataset):
                     filename, label = line_split
                     label = int(label)
                     if 0 <= label <= 10:
-                        # label = 0
+                        label=label
+                        # emb = np.mean(, axis=0)
+                        # emb=self.embeddings[0]
                         # if 0 <= label <= 10:
-                        label = 0
-                        emb = np.mean(self.embeddings[0:11], axis=0)
-                    elif 11 <= label <= 23:
-                        label = 1
-                        emb = np.mean(self.embeddings[11:24], axis=0)
-                    elif 24 <= label <= 31:
-                        label = 2
-                        emb = np.mean(self.embeddings[24:32], axis=0)
-                    elif 32 <= label <= 37:
-                        label = 3
-                        emb = np.mean(self.embeddings[32:38], axis=0)
-                    elif 38 <= label <= 47:
-                        label = 4
-                        emb = np.mean(self.embeddings[38:48], axis=0)
-                    elif 48 <= label <= 51:
-                        label = 5
-                        emb = np.mean(self.embeddings[48:52], axis=0)
-                    # else:
-                    #     continue
+                        
+                        # label = 0
+                    # elif:
+                    #     label=1
+                    #     emb = np.mean(self.embeddings[0:11], axis=0)
+                    # elif 11 <= label <= 23:
+                    #     label=1
+                    #     # emb = np.mean(, axis=0)
+                    #     emb=self.embeddings[11]
+                    # elif 24 <= label <= 31:
+                    #     label=2
+                    #     # emb = np.mean(, axis=0)
+                    #     emb=self.embeddings[24]
+                    # elif 32 <= label <= 37:
+                    #    label=3
+                    #     # emb = np.mean(, axis=0)
+                    #    emb=self.embeddings[32]
+                    # elif 38 <= label <= 47:
+                    #    label=4
+                    #     # emb = np.mean(, axis=0)
+                    #    emb=self.embeddings[38]
+                    # if 48 <= label <= 51:
+                    #     label=5
+                    #     # emb = np.mean(, axis=0)
+                    #     emb=self.embeddings[48]
+                    else:
+                        continue
                 if self.data_prefix is not None:
                     filename = osp.join(self.data_prefix, filename)
-                # emb=self.embeddings[label]
+                emb=self.embeddings[label]
                 video_infos.append(dict(filename=filename, label=label,emb=emb))
         return video_infos
